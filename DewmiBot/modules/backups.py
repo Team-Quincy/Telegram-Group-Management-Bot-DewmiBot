eb2b0d8ca58c1b53cf0e0c9dc977418cb9d0ffe6
Team-Quincy <95290845+Team-Quincy@users.noreply.github.com>
2021-12-01 09:30:47 +0530

    Update backups.py

--- DewmiBot/modules/backups.py
+++ DewmiBot/modules/backups.py
@@ -377,12 +377,12 @@ __mod_name__ = "Backups 📦"
 __help__ = """
 *Only for group owner:*
 
- 👉 `/import`: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
+ 🍎 `/import`: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
  Note that files / photos cannot be imported due to telegram restrictions.
 
- 👉 `/export`: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
+ 🍇 `/export`: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
  
- @dewmibot
+ @Missquincybot
 """
 
 IMPORT_HANDLER = CommandHandler("import", import_data)
